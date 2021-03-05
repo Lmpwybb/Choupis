6f41e336b7de2716f1833f7f72b01af3531c1464
Lmpwybb <cartoux.sebastien@orange.fr>
2021-03-05 11:31:34 +0100

    Standalone connection

--- connection.php
+++ connection.php
@@ -1,14 +1,18 @@
 <?php
 
+require "config.php";
+
 function getConnection(): ?PDO
 {
-  $connection = null;
-  try {
-        $connection = new PDO("mysql:host=?; dbname=?; charset=utf8", "?", "?");
+    global $config;
+    $connection = null;
+    try {
+        $connection = new PDO("mysql:host=" . $config['host'] . "; dbname=" . $config['dbname'] . "; 
+        charset=utf8", "" . $config['username'] . "", "" . $config['password'] . "");
         $connection->exec("set names utf8");
     } catch (PDOException $exception) {
-           echo "Connection error to MySQL: " . $exception->getMessage();
+       echo "Connection error to MySQL: " . $exception->getMessage();
     }
-        
+
     return $connection;
 }
